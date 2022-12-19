a25822c1b2e79528802adf2017446bc2b8ec5bb2
Simon Allfrey <simon.allfrey@gmail.com>
2022-12-19 13:06:59 +0100

    add strava scraper

---
+++ WebScraper.js
@@ -0,0 +1,157 @@
+const puppeteer = require( 'puppeteer' )
+
+async function getCookies( login, password) {
+
+  // const herokuDeploymentParams = {'args' : ['--no-sandbox', '--disable-setuid-sandbox']}
+  console.log("Opening the browser......");
+
+  const browser = await puppeteer.launch({
+         headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            'ignoreHTTPSErrors': true
+        })
+
+  console.log('Opened the browser2.')
+
+  // Авторизация на  www.strava.com/login
+  const page1 = await browser.newPage()
+  //await page1.setViewport({width: 1280, height: 1024})
+  await page1.goto('https://www.strava.com/login', {waitUntil: 'networkidle2'})
+  // await page1.screenshot({path: '01_login_page_loaded.png'});
+  console.log(`In getCookies: Setting login to :${login}`)
+  console.log(`In getCookies: Setting password to :${password}`)
+
+  await page1.waitForSelector('form')
+  await page1.type('input#email', login)
+  await page1.type('input#password', password)
+  // await page1.screenshot({path: '02_login_and_password_inserted.png'});
+
+  await page1.waitFor(200)
+  await page1.evaluate(()=>document
+    .querySelector('button#login-button')
+    .click()
+  )
+  await page1.waitForNavigation()
+  // await page1.screenshot({path: '03_redirected_to_new_page.png'});
+
+  // Извлекаем _strava4_session cookie
+  const sessionFourCookie = await page1.cookies()
+  // console.log(sessionFourCookie)
+  // console.log("================================")
+
+  // Авторизация на heatmap-external-a.strava.com/auth
+  const page2 = await browser.newPage()
+  await page2.setCookie(...sessionFourCookie)
+  await page2.goto('https://heatmap-external-a.strava.com/auth')
+//  await page2.goto('https://www.strava.com/heatmap')
+
+
+  // Извлекаем дополненные CloudFront cookies
+  const cloudfontCookie = await page2.cookies()
+  // await page2.screenshot({path: '04_redirected_to_heatmap_page.png'});
+  // console.log(cloudfontCookie)
+
+  // console.log('Browser end')
+  function uniq(a) {
+    let seen = {}
+    return a.filter(e => seen.hasOwnProperty(e.name) ? false : (seen[e.name]=true))
+  }
+
+  let allCookies = sessionFourCookie
+      .concat(cloudfontCookie)
+  console.log("allCookies")
+  console.log(allCookies)
+
+  let uniqueCookies = uniq(allCookies)
+  console.log("uniqueCookies")
+  console.log(uniqueCookies)
+
+//   let stringOfCookies = uniqueCookies
+// //      .filter(e => e.name.match(/^(CloudFront-Signature|CloudFront-Key-Pair-Id|CloudFront-Policy)$/))
+//       .map(e => ` ${e.name}=${e.value};`)
+//       .reduce(((s,v)=> s+v),"")
+//   console.log(stringOfCookies)
+
+  let signature = uniqueCookies
+      .filter(e => e.name.match(/^CloudFront-Signature$/))
+      .map(e => e.value)
+      .reduce(((s,v)=> s+v),"")
+  console.log(signature)
+  console.log("OrWeCouldTry")
+  console.log(uniqueCookies.find(e => e.name === 'CloudFront-Signature').value)
+  let keypairid = uniqueCookies
+      .filter(e => e.name.match(/^CloudFront-Key-Pair-Id$/))
+      .map(e => e.value)
+      .reduce(((s,v)=> s+v),"")
+  console.log(keypairid)
+  let  policy= uniqueCookies
+      .filter(e => e.name.match(/^CloudFront-Policy$/))
+      .map(e => e.value)
+      .reduce(((s,v)=> s+v),"")
+  console.log(policy)
+
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>
+<providers>
+	<provider id="10902" type="0" visible="true" background="-1">
+		<name>Simon Strava</name>
+		<mode>Simon Global Heatmap All</mode>
+		<countries>World</countries>
+		<url><![CDATA[https://heatmap-external-c.strava.com/tiles-auth/all/hot/{z}/{x}/{y}.png?v={ts}&Policy=${policy}&Key-Pair-Id=${keypairid}&Signature=${signature}]]></url>
+		<zoomPart>{z}-8</zoomPart>
+		<zoomMin>8</zoomMin>
+		<zoomMax>23</zoomMax>
+		<tileSize>256</tileSize>
+		<tileScale dpi="0" multi="2.0" replace="19" />
+		<attribution>
+			<![CDATA[© 2019 Strava]]>
+		</attribution>
+		<extraHeader>
+			<![CDATA[ Connection#keep-alive ]]>
+		</extraHeader>
+		<extraHeader>
+			<![CDATA[ Referer#https://www.strava.com/heatmap ]]>
+		</extraHeader>
+		<extraHeader>
+			<![CDATA[ User-Agent#Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36 ]]>
+		</extraHeader>
+	</provider>
+</providers>
+`
+
+//   let oldxml = `<?xml version="1.0" encoding="UTF-8"?>
+// <providers>
+// 	<provider id="10902" type="0" visible="true" background="-1">
+// 		<name>Simon Strava</name>
+// 		<mode>Simon Global Heatmap All</mode>
+// 		<countries>World</countries>
+// 		<url><![CDATA[https://heatmap-external-c.strava.com/tiles-auth/all/hot/{z}/{x}/{y}.png?v={ts}]]></url>
+// 		<zoomPart>{z}-8</zoomPart>
+// 		<zoomMin>8</zoomMin>
+// 		<zoomMax>22</zoomMax>
+// 		<tileSize>256</tileSize>
+// 		<tileScale dpi="0" multi="2.0" replace="19" />
+// 		<attribution>
+// 			<![CDATA[© 2019 Strava]]>
+// 		</attribution>
+// 		<extraHeader>
+// 			<![CDATA[ Cookie#{${stringOfCookies}} ]]>
+// 		</extraHeader>
+// 		<extraHeader>
+// 			<![CDATA[ Connection#keep-alive ]]>
+// 		</extraHeader>
+// 		<extraHeader>
+// 			<![CDATA[ Referer#https://www.strava.com/heatmap ]]>
+// 		</extraHeader>
+// 		<extraHeader>
+// 			<![CDATA[ User-Agent#Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36 ]]>
+// 		</extraHeader>
+// 	</provider>
+// </providers>
+// `
+
+  console.log(xml)
+  await browser.close()
+  return xml
+}
+
+module.exports = getCookies
--- app.js
+++ app.js
@@ -1,59 +1,39 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const fs = require ('fs')
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const rateLimit = require('express-rate-limit')
+const scraper = require( './WebScraper.js' )
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.type('html').send(html));
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .get( '/sa/:login/:password/', async ( req, res, next ) => {
+    console.log("In async")
+      const login = req.params.login
+      const password = req.params.password
+      if (!login) return next(error(400, 'No login parameter'))
+      if (!password) return next(error(400, 'No password parameter'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    console.log("In async: awaiting scraper")
+      const xml = await scraper(login, password)
+//      fs.writeFile("./result.xml", xml, function(err) {
+//        if(err) {
+//            return console.log(err);
+//        }
+//        console.log("./result.xml was saved")
+//      })
+//    console.log('writing reslult.xml')
+ //     await fs.writeFileSync('./result.xml',xml)
+  //  console.log('written result.xml')
+   //   await res.download('./result.xml')
+      res.setHeader("Content-Type", "text/plain")
+      res.send(xml)
+      res.end("")
+      // res.json(authedCookies);
+    })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
